cce2950b5e97a8c9950f999498f019eea5fe6cb5
Firoz193 <firoz.ewu.bd@gmail.com>
2026-08-06 02:31:08 +0600

    ES-6 add

---
+++ ES-6/template-literals.js
@@ -0,0 +1,26 @@
+let age = 5;
+let name = "Firoz";
+
+// console.log(`My name is ${name} and age is ${age}..`)
+// console.log(`hay`)
+
+let x = 4
+let y = 8
+
+// console.log(`Total sum is: ${x+y}`)
+
+function add(a, b){
+    return a+b;
+}
+
+// console.log(`Total sum is: ${add(565,2)}`)
+
+let text = `    Hay!
+    this is firoz.
+    What about you.`
+
+    // console.log(text)
+
+let arr = [1,2,3]
+
+console.log(`array is: ${arr}`)
